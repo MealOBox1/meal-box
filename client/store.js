3b84305177d5b96cd7218fb45a7a46f62a1729f8
prashant_tongra <prashant@foodmonk.com>
2020-07-01 09:19:21 +0530

    better

--- client/store.js
+++ client/store.js
@@ -1,47 +1,47 @@
-// import { createStore, applyMiddleware, compose } from "redux";
-// import thunk from "redux-thunk";
-// import rootReducer from "./reducers/index";
-// // import { autoRehydrate } from "redux-persist";
-
-// const initialState = {};
-
-// const middelware = [thunk];
-// const composeEnhancers =
-//   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-//         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-//       })
-//     : compose;
-
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   composeEnhancers(
-//     applyMiddleware(...middelware)
-//     // autoRehydrate()
-//     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// );
-
-// export default store;
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
+// import { autoRehydrate } from "redux-persist";
 
 const initialState = {};
 
 const middelware = [thunk];
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middelware),
-    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(...middelware)
+    // autoRehydrate()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
 export default store;
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import rootReducer from "./reducers/index";
+
+// const initialState = {};
+
+// const middelware = [thunk];
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middelware),
+//     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+// export default store;
 
 
 
